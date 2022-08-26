e460517f585deee929ebf52023a2ecdb729bda9e
Komsan Nurak <n.komsan@hotmail.com>
2022-08-26 11:56:39 +0700

    Modify config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost:8008/ci3-login/';
+$config['base_url'] = 'http://localhost/ci3-login/';
 
 /*
 |--------------------------------------------------------------------------
